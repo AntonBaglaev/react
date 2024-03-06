1833ed61dd26d095de452f7e22d62eaad25fcd03
Anton Baglaev <abaglaev@yandex.ru>
2024-03-06 16:11:23 +0300

    jsx

--- js/index.js
+++ js/index.js
@@ -1,4 +1,23 @@
-ReactDOM.render(<div>
-    <h1>Привет</h1>
-    <h2>Hello</h2>
-</div>, document.getElementById("app"))
+// ReactDOM.render(React.createElement('input', {
+//     placeholder: "Help text",
+//     onClick: () => console.log("Clicked"),
+//     onMouseEnter: () => console.log("Mouse Over"),
+// }), document.getElementById("app"))
+
+// то же самое но с JSX
+
+const inputClick = () => console.log("Clicked")
+const mouseOver = () => console.log("Mouse Over")
+
+const helpText = "Help text!"
+
+const elements =(<div className="name">
+        <h1>{helpText}</h1>
+        <input placeholder={helpText} 
+        onClick={inputClick} onMouseEnter={mouseOver} />
+        <p>{helpText == "Help text!" ? "Yes" : "No"}</p>
+    </div>) 
+
+const app = document.getElementById("app")
+
+ReactDOM.render(elements, app)
