bf2b3fa557852ef55b15c7791f8a8f5d45bac3a6
Anton Baglaev <abaglaev@yandex.ru>
2024-03-11 17:40:48 +0300

    Разделили по файлам

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Header from "./components/Header"
+
+class App extends React.Component {
+    helpText = "Help text!"
+    render() {
+        return (<div className="name">
+            <Header title="Шапка сайта" />
+            <h1>{this.helpText}</h1>
+            <input placeholder={this.helpText}
+                onClick={this.inputClick} onMouseEnter={this.mouseOver} />
+            <p>{this.helpText === "Help text!" ? "Yes" : "No"}</p>
+        </div>)
+    }
+
+    inputClick() { console.log("Clicked") }
+    mouseOver() { console.log("Mouse Over") }
+}
+
+export default App
