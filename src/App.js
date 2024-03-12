b18b0c269cb6f5ea47deabc17be3b2c44e18cfe2
Anton Baglaev <abaglaev@yandex.ru>
2024-03-12 17:02:51 +0300

    Работа с хуками

--- src/App.js
+++ src/App.js
@@ -14,6 +14,11 @@ class App extends React.Component {
         this.inputClick = this.inputClick.bind(this)
     }
 
+    componentDidUpdate(prevProp) {
+        if(this.state.helpText !== "Help")
+            console.log("Some")
+    }
+
     render() {
         return (<div className="name">
             <Header title="Шапка сайта" />
