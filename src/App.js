486d1fd7ebb25c18a4282310b76d43084b1d55f4
Anton Baglaev <abaglaev@yandex.ru>
2024-03-13 18:12:31 +0300

    Работа с списками / Вывод страницы с пользователями

--- src/App.js
+++ src/App.js
@@ -1,42 +1,22 @@
 import React from "react"
 import Header from "./components/Header"
-import Image from "./components/Image"
-import logo from "./img/logo.png"
+import Users from "./components/Users"
 
 class App extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            helpText: "Help text",
-            userData: "",
-        }
 
-        this.inputClick = this.inputClick.bind(this)
-    }
-
-    componentDidUpdate(prevProp) {
-        if(this.state.helpText !== "Help")
-            console.log("Some")
-    }
 
     render() {
-        return (<div className="name">
-            <Header title="Шапка сайта" />
-            <h1>{this.state.helpText}</h1>
-            <h2>{this.state.userData}</h2>
-            <input placeholder={this.state.helpText}
-                onChange={event => this.setState({ userData: event.target.value })}
-                onClick={this.inputClick} onMouseEnter={this.mouseOver} />
-            <p>{this.state.helpText === "Help text!" ? "Yes" : "No"}</p>
-            <Image image={logo} />
+        return (<div>
+            <Header title="Список пользователей" />
+            <main>
+                <Users />
+            </main>
+            <aside>
+                
+            </aside>
         </div>)
     }
 
-    inputClick() {
-        this.setState({ helpText: "Changed" })
-        console.log("Clicked")
-    }
-    mouseOver() { console.log("Mouse Over") }
 }
 
 export default App
