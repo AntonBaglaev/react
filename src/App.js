8cd13c3362d82abced9ea23f3dafa32ce34bc3a7
Anton Baglaev <abaglaev@yandex.ru>
2024-03-18 15:36:34 +0300

    Работа с формами

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
 import React from "react"
 import Header from "./components/Header"
 import Users from "./components/Users"
+import AddUser from "./components/AddUser"
 
 class App extends React.Component {
-
-
+    constructor(props) {
+        super(props)
+        this.state = {
+            users: [
+                {
+                    id: 1,
+                    firstname: 'Bob',
+                    lastname: 'Marley',
+                    bio: 'Lorem ipsum dolor sit amet consecteure',
+                    age: 40,
+                    isHappy: true
+                },
+                {
+                    id: 2,
+                    firstname: 'John',
+                    lastname: 'Doe',
+                    bio: 'Lorem ipsum dolor sit amet consecteure',
+                    age: 22,
+                    isHappy: false
+                }
+            ]
+        }
+        this.addUser = this.addUser.bind(this)
+    }
     render() {
         return (<div>
             <Header title="Список пользователей" />
             <main>
-                <Users />
+                <Users users={this.state.users} />
             </main>
             <aside>
-                
+                < AddUser onAdd={this.addUser} />
             </aside>
         </div>)
+    }
+
+    addUser(user) {
+        const id = this.state.users.length + 1
+        this.setState({ users: [...this.state.users, {id, ...user}] })
     }
 
 }
